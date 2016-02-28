dc3c50828d096795ed9a93fc71f52bf25af83e80
DarkPark <darkpark@pisem.net>
2016-02-28 16:09:38 +0200

    move config and media files from the spasdk package

---
+++ config.js
@@ -0,0 +1,43 @@
+/**
+ * Common configuration for plugins.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var path = require('path');
+
+
+// root SPA config
+// to be extended in other plugins
+module.exports = {
+    // directory to look for source files
+    source: 'src',
+
+    // directory to store output files
+    target: 'app',
+
+    // base port
+    //port: 8000,
+
+    // info channels
+    notifications: {
+        console: {
+            info: true,
+            warn: true,
+            fail: true
+        },
+        popup: {
+            info: {show: false, icon: path.join(__dirname, 'media', 'info.png')},
+            warn: {show: true,  icon: path.join(__dirname, 'media', 'warn.png')},
+            fail: {show: true,  icon: path.join(__dirname, 'media', 'fail.png')}
+        },
+        sound: {
+            info: {play: false, file: path.join(__dirname, 'media', 'info.wav')},
+            warn: {play: false, file: path.join(__dirname, 'media', 'warn.wav')},
+            fail: {play: false, file: path.join(__dirname, 'media', 'fail.wav')}
+        }
+    }
+};
